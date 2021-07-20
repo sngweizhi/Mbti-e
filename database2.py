2a9004a3027d9960b92c17aaa2b47f728cf2d632
sngweizhi <sngweizhi@gmail.com>
2021-07-21 00:16:55 +0800

    Update database2.py

--- database2.py
+++ database2.py
@@ -301,7 +301,7 @@ def get_gender_chat(gender, gendermatch, seeking):
         else:
             return [0,0,0,0,0]
     elif gendermatch == 'Any':
-        user = Queue.query.filter(Queue.seeking==seeking).filter((Queue.gendermatch==gender | Queue.gendermatch==gendermatch)).first()
+        user = db.session.query(Queue).filter(Queue.seeking==seeking).filter((Queue.gendermatch==gender | Queue.gendermatch==gendermatch)).first()
         if user != None:
             user_info = [user.chat_id,user.gender,user.gendermatch,user.seeking,user.mbti]
             return user_info

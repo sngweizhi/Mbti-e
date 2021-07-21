64614073b4dbcaff7490303ae50378a5f0922609
sngweizhi <sngweizhi@gmail.com>
2021-07-22 03:37:54 +0800

    Update messages.py

--- messages.py
+++ messages.py
@@ -7,8 +7,8 @@ Users do not get to see each others' profile pictures, hence allowing conversati
 One chat at a time*: The bot only permits each person to have one conversation at a time, thereby allowing users to focus their attention on their one match\.\n\n*3\.\
 MBTI matching algorithm*: The Myers Briggs Type Indicator is a well\-known personality profiling instrument that categorizes people into 16 different personality\
 types based on how they perceive the world and make decisions\. The bot will match you with others who have a compatible personality \
-type\.\n\nUse these commands control the bot:\n\n/setup \- Setup or configure your profile\n/start \- start matching with someone\n/icebreaker \- send a 2 Truth 1 \
-Lie poll to your match\!\n/help \- for more info on how to use the bot\."
+type\.\n\n*Use these commands control the bot:*\n/setup \- Setup or configure your profile\n/start \- start matching with someone\n/icebreaker \- send a 2 Truth 1 \
+Lie poll to your match\!\n/report \- to report a user\.\n/help \- for more info on how to use the bot\."
 
 help = '*Help*: \n\nUse '
 

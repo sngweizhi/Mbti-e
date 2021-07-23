f6c1eef89daebd2830894a7492eca8fddfe158b9
sngweizhi <sngweizhi@gmail.com>
2021-07-24 00:44:55 +0800

    Update messages.py

--- messages.py
+++ messages.py
@@ -1,17 +1,17 @@
 
 
-welcome = "Welcome to *MBTInder Bot SG*, an anonymous chatbot for dating and making friends based on an MBTI matching algorithm\. \
+welcome = "Welcome to *MBTInder Bot*, an anonymous chatbot for dating and making friends based on an MBTI matching algorithm\. \
 While MBTInder is a portmanteau of MBTI and Tinder, it seeks to address what tinder fails to do \
 — *forge meaningful relationships*\. It does so in three ways:\n\n*1\. Anonymous chat*: \
 Users do not get to see each others' profile pictures, hence allowing conversations to be more genuine and less shallow\.\n\n*2\. \
 One chat at a time*: The bot only permits each person to have one conversation at a time, thereby allowing users to focus their attention on their one match\.\n\n*3\.\
 MBTI matching algorithm*: The Myers Briggs Type Indicator is a well\-known personality profiling instrument that categorizes people into 16 different personality\
 types based on how they perceive the world and make decisions\. The bot will match you with others who have a compatible personality \
-type\.\n\n*Use these commands control the bot:*\n/setup \- Setup or configure your profile\n/start \- start matching with someone\n/stop \- End the chat\n/icebreaker \- send a 2 Truth 1 \
-Lie poll to your match\!\n/report \- to report a user\n/feedback \- tell us how we can improve the bot\!\n/help \- for more info on how to use the bot\."
+type\.\n\n*Use these commands control the bot:*\n/setup \- Setup or configure your profile\n/start \- Start matching\!\n/stop \- End the chat\n/icebreaker \- Send a 2 Truth 1 \
+Lie game to your match\!\n/report \- Report a user for any misconduct\.\n/feedback \- tell us how we can improve the bot\!"
 
-help = '*Help*\n\nThis section is being updated\. If you need help, contact the admin through the feedback function\!\n\n*List of commands:*\n/setup \- Setup or configure your profile\n/start \- Start matching\!\n/stop \- End the chat\n/icebreaker \- send a 2 Truth 1 \
-Lie poll to your match\!\n/report \- to report a user for any misconduct\.\n/feedback \- tell us how we can improve the bot\!'
+help = '*Help*\n\nThis section is being updated\. If you need help, contact the admin through the feedback function\!\n\n*List of commands:*\n/setup \- Setup or configure your profile\n/start \- Start matching\!\n/stop \- End the chat\n/icebreaker \- Send a 2 Truth 1 \
+Lie game to your match\!\n/report \- Report a user for any misconduct\.\n/feedback \- Tell us how we can improve the bot\!'
 
 search_sticker = 'CAACAgUAAxkBAAIJxmDyqgXaYH7ZcxzHGZbOjwtaEqMAA54CAAKJ_JBXZilybC94gzggBA'
 match_sticker ='CAACAgUAAxkBAAIJ7WDyykDX_M8j0z3TCKanMmuDUt2tAAKOAwACPyqZV1g5tYcaYUa6IAQ'

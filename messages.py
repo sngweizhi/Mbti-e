42df9f9aa1c503c5c13befb766b3e39fbe87009c
sngweizhi <sngweizhi@gmail.com>
2021-07-24 00:39:13 +0800

    Update messages.py

--- messages.py
+++ messages.py
@@ -7,11 +7,11 @@ Users do not get to see each others' profile pictures, hence allowing conversati
 One chat at a time*: The bot only permits each person to have one conversation at a time, thereby allowing users to focus their attention on their one match\.\n\n*3\.\
 MBTI matching algorithm*: The Myers Briggs Type Indicator is a well\-known personality profiling instrument that categorizes people into 16 different personality\
 types based on how they perceive the world and make decisions\. The bot will match you with others who have a compatible personality \
-type\.\n\n*Use these commands control the bot:*\n/setup \- Setup or configure your profile\n/start \- start matching with someone\n/icebreaker \- send a 2 Truth 1 \
-Lie poll to your match\!\n/report \- to report a user\.\n/help \- for more info on how to use the bot\."
+type\.\n\n*Use these commands control the bot:*\n/setup \- Setup or configure your profile\n/start \- start matching with someone\n/stop \- End the chat\n/icebreaker \- send a 2 Truth 1 \
+Lie poll to your match\!\n/report \- to report a user\n/feedback \- tell us how we can improve the bot\!\n/help \- for more info on how to use the bot\."
 
-help = '*Help*\n\nThis section is being updated\. If you need help, contact the admin through the feedback function\!\n\n*List of commands:*\n/setup \- Setup or configure your profile\n/start \- start matching with someone\n/icebreaker \- send a 2 Truth 1 \
-Lie poll to your match\!\n/report \- to report a user\.\n/help \- for more info on how to use the bot\.'
+help = '*Help*\n\nThis section is being updated\. If you need help, contact the admin through the feedback function\!\n\n*List of commands:*\n/setup \- Setup or configure your profile\n/start \- Start matching!\n/stop \- End the chat\n/icebreaker \- send a 2 Truth 1 \
+Lie poll to your match\!\n/report \- to report a user for any misconduct\.\n/feedback \- tell us how we can improve the bot\!'
 
 search_sticker = 'CAACAgUAAxkBAAIJxmDyqgXaYH7ZcxzHGZbOjwtaEqMAA54CAAKJ_JBXZilybC94gzggBA'
 match_sticker ='CAACAgUAAxkBAAIJ7WDyykDX_M8j0z3TCKanMmuDUt2tAAKOAwACPyqZV1g5tYcaYUa6IAQ'
